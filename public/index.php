7261e2f2875cff21689072e0dc32f99a0615aff6
Sylvain Aubert <sylvain@atelierdixdouze.com>
2019-04-13 12:08:28 +0200

    creation of products table / Twig implement

--- public/index.php
+++ public/index.php
@@ -1,1 +1,10 @@
 <?php
+
+require '../vendor/autoload.php';
+
+$products = ['Ours', 'Orignal', 'Caribou', 'Tétra', 'Carcajou'];
+
+$loader = new \Twig\Loader\FilesystemLoader('/../src/View');
+$twig = new \Twig\Environment($loader);
+
+echo $twig->render('index.html.twig');
